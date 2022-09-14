784aad5e356bb8cea5b560ee26c43e65fe2dcb36
Abram <israelvictory87@gmail.com>
2022-09-14 23:43:43 +0100

    Import Explore component

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,27 @@
   <Navbar />
   <!-- End of Navbar -->
 
+  <!-- Explore -->
+  <Explore />
+  <!-- End of Explore -->
+
+  <!-- Release -->
+  <Release />
+  <!-- End of Release -->
+
   <router-view />
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import Explore from "@/components/Explore.vue";
+import Release from "@/components/Release.vue";
 
 export default {
   name: "App",
   components: {
-    Navbar,
+    Navbar, Explore,
+    Release
   }
 }
 </script>
@@ -23,6 +34,27 @@ export default {
 * {
   margin: 0px;
   padding: 0px;
+}
+
+body {
+  scroll-behavior: smooth;
+}
+
+body::-webkit-scrollbar {
+  width: 15px;
+}
+
+body::-webkit-scrollbar-track {
+  background: #374151;
+}
+
+body::-webkit-scrollbar-thumb {
+  background-color: #262629;
+  /* color of the scroll thumb */
+  border-radius: 20px;
+  /* roundness of the scroll thumb */
+  border: 3px solid #192026;
+  /* creates padding around scroll thumb */
 }
 
 #app {
