0699b0232f164e36e96d72c8ac73c6f7ad6ee70b
Abram <israelvictory87@gmail.com>
2022-09-16 00:15:35 +0100

    Created and include props to img

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="movie-card">
+        <img :src=source :alt=text class="w-100 h-100 img-responsive">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MovieCard",
+    props: {
+        source: String,
+        text: String
+    }
+}
+</script>
